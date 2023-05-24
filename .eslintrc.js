508b08714611b480849b242537aafff409cf41fc
Goran Markovic <goran.markovic@riddleandcode.com>
2023-05-24 16:40:57 +0200

    rm space

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['eslint:recommended',
-        'airbnb-base', 
+        'airbnb-base',
         'plugin:import/recommended',
         'plugin:import/recommended',
         'plugin:import/typescript'],
